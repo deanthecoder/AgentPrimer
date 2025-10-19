45499ef5ca145f8389b1ed318423165b3d6e5681
Dean Edis <dean.edis@gmail.com>
2025-10-19 18:28:50 +0100

    Feature: First batch of functionality.

---
+++ AgentPrimer/AgentPrimer/AppSettings.cs
@@ -0,0 +1,30 @@
+// Code authored by Dean Edis (DeanTheCoder).
+// Anyone is free to copy, modify, use, compile, or distribute this software,
+// either in source code form or as a compiled binary, for any non-commercial
+// purpose.
+// 
+// If you modify the code, please retain this copyright header,
+// and consider contributing back to the repository or letting us know
+// about your modifications. Your contributions are valued!
+// 
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND.
+using DTC.Core.Settings;
+
+namespace AgentPrimer;
+
+public class AppSettings : UserSettingsBase
+{
+    /// <summary>
+    /// Cached collection of nuget package 'PackageID|Description'.
+    /// </summary>
+    public List<string> NugetCache
+    {
+        get => Get<List<string>>();
+        set => Set(value);
+    }
+    
+    protected override void ApplyDefaults()
+    {
+        NugetCache = [];
+    }
+}
